bf878682dfbcafc98bac254a53e03a1f50b980c8
sedofiameshack-creator <msedofia@byupathway.edu>
2026-07-04 09:59:32 +0000

    week01/Exercise3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random random = new Random();
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+        {
+            int magicNumber = random.Next(1, 101); // 1 to 100 inclusive
+            int guess;
+            int guessCount = 0;
+
+            Console.Write("What is your guess? ");
+            guess = Convert.ToInt32(Console.ReadLine());
+            guessCount++;
+
+            while (guess != magicNumber)
+            {
+                if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                Console.Write("What is your guess? ");
+                guess = Convert.ToInt32(Console.ReadLine());
+                guessCount++;
+            }
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"It took you {guessCount} guesses.");
+
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine();
+        }
     }
 }
